c48eb799d9b12efc30f885e7446ad980a57d7e7e
María Laura Escanes <129324078+EscanesMLaura@users.noreply.github.com>
2023-12-14 12:59:59 -0300

    main

--- src/components/Candidato.jsx
+++ src/components/Candidato.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 
 const Candidato = ({ valor, index, onBuscarUno, onGuardarUno }) => {
 
-    const handleClick = (event) => {
-        event.preventDefault();
-    };
-
     return (
         <>
             <div className='usuario'>
@@ -15,8 +11,8 @@ const Candidato = ({ valor, index, onBuscarUno, onGuardarUno }) => {
                 <div className='datos'>
                     <div className='nombre'>{valor.name.title}. {valor.name.first} {valor.name.last}</div>
                     <div> Ubicación: {valor.location.city} ({valor.location.country})</div>
-                    <div><a href="#" onClick={handleClick}>{valor.email}</a></div>
-                    <div><a href="#" onClick={handleClick}>{valor.phone}</a></div>
+                    <div><a href="#">{valor.email}</a></div>
+                    <div><a href="#">{valor.phone}</a></div>
                 </div>
                 <div className='botones'>
                     <button onClick={() => onBuscarUno(index)} className='rojo'>Ocultar</button>
